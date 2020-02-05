2a8dd313d6157ec14038be9493a8e74778bf8804
Paweł Szynal <pawel.szynal@email.com>
2020-02-05 20:23:41 +0100

    Create LocalShare.ps1

---
+++ LocalShare.ps1
@@ -0,0 +1,10 @@
+# Pawel Szynal
+
+New-NetIPAddress -InterfaceIndex 4 -IPAddress 192.168.1.2 -PrefixLength 24 -DefaultGateway 192.168.1.1
+Set-NetConnectionProfile -Name "PC_Szynal" -NetworkCategory Private
+
+
+# Krzysztof Sobocinski
+# New-NetIPAddress -InterfaceIndex 4 -IPAddress 192.168.1.3 -PrefixLength 24 -DefaultGateway 192.168.1.1
+# Set-NetConnectionProfile -Name "PC_Sobocinski" -NetworkCategory Private
+
