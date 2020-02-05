371e4d212a23f0dc4f175f5337597f689589438e
Krzysztof Sobocinski <kszys96@outlook.com>
2020-02-05 22:50:52 +0100

    Ethernet script index 4 to 'ethernet'

--- LocalShare.ps1
+++ LocalShare.ps1
@@ -7,7 +7,7 @@ $id = Read-Host -Prompt 'Get id'
 if ($id -eq 1) {
 
     Write-Output "Set for Krzysztof Sobocinski";
-    New-NetIPAddress -InterfaceIndex 4 -IPAddress 192.168.1.3 -PrefixLength 24 -DefaultGateway 192.168.1.1
+    New-NetIPAddress -InterfaceAlias 'Ethernet' -IPAddress 192.168.1.3 -PrefixLength 24 -DefaultGateway 192.168.1.1
     Start-Sleep -s 7
     Set-NetConnectionProfile -NetworkCategory Private
     Write-Output "Done!";
--- ResetEthernet.ps1
+++ ResetEthernet.ps1
@@ -1,7 +1,7 @@
 Set-NetConnectionProfile -NetworkCategory Public
-Set-NetIPInterface -InterfaceIndex 4 -Dhcp Enabled
+Set-NetIPInterface -InterfaceAlias 'Ethernet' -Dhcp Enabled
 Start-Sleep -s 5
-Get-NetIPAddress -InterfaceIndex 4 | Remove-NetRoute
+Get-NetIPAddress -InterfaceAlias 'Ethernet' | Remove-NetRoute
 netsh interface set interface "Ethernet" DISABLED
 netsh interface set interface "Ethernet" ENABLED
 Set-ExecutionPolicy Restricted
