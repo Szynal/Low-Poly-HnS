f0cb3d2c518f6ef64ce5eb4e412649e0275d8459
Paweł Szynal <pawel.szynal@email.com>
2020-02-05 23:04:03 +0100

    Update LocalShare.ps1

--- LocalShare.ps1
+++ LocalShare.ps1
@@ -8,14 +8,14 @@ if ($id -eq 1) {
 
     Write-Output "Set for Krzysztof Sobocinski";
     New-NetIPAddress -InterfaceAlias 'Ethernet' -IPAddress 192.168.1.3 -PrefixLength 24 -DefaultGateway 192.168.1.1
-    Start-Sleep -s 7
+    Start-Sleep -s 20
     Set-NetConnectionProfile -NetworkCategory Private
     Write-Output "Done!";
 } elseif ($id -eq 2) {
 
     Write-Output "Set for Pawel Szynal";
     New-NetIPAddress -InterfaceAlias 'Ethernet' -IPAddress 192.168.1.2 -PrefixLength 24 -DefaultGateway 192.168.1.1
-    Start-Sleep -s 7
+    Start-Sleep -s 20
     Set-NetConnectionProfile -NetworkCategory Private
     Write-Output "Done!";
 }else{
