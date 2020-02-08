03ca7bb367acc157390fdd2100db32883d9bf01e
Paweł Szynal <pawel.szynal@email.com>
2020-02-08 02:03:14 +0100

    Create CharacterAnimatorManger.cs

---
+++ Assets/Scripts/Character/CharacterAnimatorManger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace LowPolyHnS
+{
+    [RequireComponent(typeof(Animator))]
+
+    public class CharacterAnimatorManger : MonoBehaviour
+    {
+        private static int _moveParameter = Animator.StringToHash("move");
+        private Animator animator;
+
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        public void AnimateCharacterMovement(bool isMoving, Vector3 motion)
+        {
+            if (animator != null)
+            {
+                animator.SetFloat("move", isMoving
+                    ? Mathf.Lerp(animator.GetFloat(_moveParameter), motion.sqrMagnitude, 10 * Time.deltaTime)
+                    : Mathf.Lerp(animator.GetFloat(_moveParameter), 0, 10 * Time.deltaTime));
+
+            }
+        }
+    }
+}
