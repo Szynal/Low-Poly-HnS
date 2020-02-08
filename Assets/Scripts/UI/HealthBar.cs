80ced96572b8783a270cc2cdf5c57519a445c46b
Paweł Szynal <pawel.szynal@email.com>
2020-02-08 16:21:51 +0100

    Create HealthBar.cs

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace LowPolyHnS
+{
+    [RequireComponent(typeof(Slider))]
+    public class HealthBar : MonoBehaviour
+    {
+        [SerializeField] private Transform cameraPosition = null;
+        [SerializeField] Slider miniHealthSlider = null;
+        private Transform miniHealthSliderPos;
+
+        private void Start()
+        {
+            if (miniHealthSlider != null)
+            {
+                miniHealthSliderPos = miniHealthSlider.transform;
+            }
+        }
+
+        private void Update()
+        {
+            RotateTowardsPlayer();
+        }
+
+        private void RotateTowardsPlayer()
+        {
+            if (cameraPosition == null || miniHealthSlider == null) return;
+            miniHealthSliderPos.LookAt(cameraPosition.position);
+        }
+    }
+}
