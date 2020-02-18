a26f952cac97b6c4560d10c0d35d7d76a4b8ca42
Paweł Szynal <pawel.szynal@email.com>
2020-02-18 23:54:28 +0100

    Added Chest Animation

---
+++ Assets/Scripts/Animation/AnimationTools.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace LowPolyHnS
+{
+    public static class AnimationTools
+    {
+        public static bool PlayAnimation(Animator animator, int stateNameHash, int parameterNameHash, bool typeOn,
+            float normalizedTime)
+        {
+            float playTime = normalizedTime % 1;
+
+            if (typeOn && normalizedTime > 1)
+            {
+                playTime = 1.0f;
+            }
+            else if (!typeOn && (normalizedTime > 1 || normalizedTime < 0))
+            {
+                playTime = 0.0f;
+            }
+
+            if (animator.GetFloat(parameterNameHash) >= 1)
+            {
+                typeOn = false;
+                animator.SetFloat(parameterNameHash, -1);
+                animator.Play(stateNameHash, -1, playTime);
+            }
+            else
+            {
+                typeOn = true;
+                animator.SetFloat(parameterNameHash, 1);
+                animator.Play(stateNameHash, -1, playTime);
+            }
+
+            return typeOn;
+        }
+    }
+}
---
+++ Assets/Scripts/Objects/Chest.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace LowPolyHnS
+{
+    [RequireComponent(typeof(Animator))]
+    public class Chest : MonoBehaviour
+    {
+        private Animator animator;
+        private static int _stateNameHash = Animator.StringToHash("ChestOpenClose");
+        private static int _chestParameter = Animator.StringToHash("Open");
+
+        private AnimatorStateInfo chestState;
+        private bool chestOn;
+
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            TEST();
+        }
+
+        private void TEST()
+        {
+            if (!Input.GetKeyDown(KeyCode.O)) return;
+            chestState = animator.GetCurrentAnimatorStateInfo(0);
+            chestOn = AnimationTools.PlayAnimation(
+                animator, _stateNameHash, _chestParameter, chestOn, chestState.normalizedTime);
+        }
+    }
+}
