13c9c8c6afd1355ea4d749e14f63556c429bc572
Paweł Szynal <pawel.szynal@email.com>
2020-03-21 15:04:04 +0100

    Update

---
+++ Assets/Scripts/System/Trigger/Trigger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace LowPolyHnS.Core
+{
+    [AddComponentMenu("LowPolyHnS/Trigger", 0)]
+    public class Trigger : MonoBehaviour
+    {
+        public enum ItemOpts
+        {
+            Conditions,
+            Actions
+        }
+
+        /*
+        [Serializable]
+        public class Item
+        {
+            public Actions actions = null;
+            public Conditions conditions = null;
+            public ItemOpts option = ItemOpts.Actions;
+
+            public Item(Actions actions)
+            {
+                option = ItemOpts.Actions;
+                this.actions = actions;
+            }
+
+            public Item(Conditions conditions)
+            {
+                option = ItemOpts.Conditions;
+                this.conditions = conditions;
+            }
+        }
+        */
+    }
+}
