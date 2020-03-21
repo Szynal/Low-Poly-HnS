09033cd4cf87ac56e3d8358df915e89c43cfdf9a
Paweł Szynal <pawel.szynal@email.com>
2020-03-21 20:28:22 +0100

    Create GameInputModule.cs

---
+++ Assets/Scripts/System/Core/Events/GameInputModule.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace LowPolyHnS.Core
+{
+    [AddComponentMenu("")]
+    public class GameInputModule : StandaloneInputModule
+    {
+        public GameObject GameObjectUnderPointer(int pointerId)
+        {
+            PointerEventData lastPointer = GetLastPointerEventData(pointerId);
+            return lastPointer?.pointerCurrentRaycast.gameObject;
+        }
+
+        public GameObject GameObjectUnderPointer()
+        {
+            return GameObjectUnderPointer(kMouseLeftId);
+        }
+    }
+}
