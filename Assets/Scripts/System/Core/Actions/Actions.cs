3d0d11bbb63cb60788b3cf76195a3cf880524113
Paweł Szynal <pawel.szynal@email.com>
2020-03-21 15:03:46 +0100

    Create Actions.cs

---
+++ Assets/Scripts/System/Core/Actions/Actions.cs
@@ -0,0 +1,25 @@
+﻿namespace LowPolyHnS.Core
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.Events;
+
+#if UNITY_EDITOR
+    using UnityEditor;
+#endif
+
+    [ExecuteInEditMode]
+    [AddComponentMenu("Game Creator/Actions", 0)]
+    public class Actions : MonoBehaviour
+    {
+        public static bool IS_BLOCKING_ACTION_RUNNING = false;
+        public class ExecuteEvent : UnityEvent<GameObject> { }
+
+        public int currentID = 0;
+        public int instanceID = 0;
+
+
+        public IActionsList actionsList;
+    }
+}
