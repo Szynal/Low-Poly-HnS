753e1c368db5c60eb1647331609311b827b8fcb9
Paweł Szynal <pawel.szynal@email.com>
2020-03-21 20:28:19 +0100

    Create HookCamera.cs

---
+++ Assets/Scripts/System/Core/Hooks/HookCamera.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace LowPolyHnS.Core.Hooks
+{
+    [AddComponentMenu("LowPolyHnS/Hooks/HookCamera", 100)]
+    public class HookCamera : Hook<HookCamera>
+    {
+    }
+}
