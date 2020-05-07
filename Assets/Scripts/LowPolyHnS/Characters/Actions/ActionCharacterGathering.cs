a7622ee48c83d5170084d2900b64aabdc390c07a
Paweł Szynal <pawel.szynal@email.com>
2020-05-07 02:51:15 +0200

    Mining 4.0

---
+++ Assets/Scripts/LowPolyHnS/Characters/Actions/ActionCharacterGathering.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using LowPolyHnS.Core;
+using LowPolyHnS.Inventory;
+using UnityEngine;
+
+namespace LowPolyHnS.Characters
+{
+    public class ActionCharacterGathering : IAction
+    {
+        private static readonly Vector3 PLANE = new Vector3(1, 0, 1);
+
+        public TargetCharacter Character = new TargetCharacter(TargetCharacter.Target.Player);
+        public TargetPosition TargetPosition = new TargetPosition();
+
+        private float duration;
+        private bool wasControllable;
+        private Character cacheCharacter;
+
+        public AnimationClip clip;
+
+        private float fadeIn = 0.1f;
+        private float fadeOut = 0.1f;
+        private bool waitTillComplete = true;
+
+        private CharacterAnimator characterAnimator;
+        private bool forceStop;
+
+        public LootTable lootTable;
+
+        public override bool InstantExecute(GameObject target, IAction[] actions, int index)
+        {
+            cacheCharacter = Character.GetCharacter(target);
+            if (cacheCharacter == null)
+            {
+                return true;
+            }
+
+            wasControllable = cacheCharacter.IsControllable();
+            cacheCharacter.characterLocomotion.SetIsControllable(false);
+
+            Vector3 rotationDirection =
+                TargetPosition.GetPosition(target) - cacheCharacter.gameObject.transform.position;
+
+            rotationDirection = Vector3.Scale(rotationDirection, PLANE).normalized;
+            duration = Vector3.Angle(cacheCharacter.transform.TransformDirection(Vector3.forward), rotationDirection) /
+                       cacheCharacter.characterLocomotion.angularSpeed;
+
+            cacheCharacter.characterLocomotion.SetRotation(rotationDirection);
+
+            return !waitTillComplete;
+        }
+
+        public override IEnumerator Execute(GameObject target, IAction[] actions, int index, params object[] parameters)
+        {
+            WaitForSeconds waitForSeconds = new WaitForSeconds(duration);
+            yield return waitForSeconds;
+
+            if (cacheCharacter != null)
+            {
+                CharacterLocomotion locomotion = cacheCharacter.characterLocomotion;
+                locomotion.SetIsControllable(wasControllable);
+
+
+                forceStop = false;
+
+                cacheCharacter.characterLocomotion.SetIsControllable(false);
+
+                if (clip != null && cacheCharacter.GetCharacterAnimator() != null)
+                {
+                    characterAnimator = cacheCharacter.GetCharacterAnimator();
+                    characterAnimator.CrossFadeGesture(
+                        clip, 1, null,
+                        fadeIn, fadeOut
+                    );
+                }
+
+                if (clip != null && cacheCharacter != null && cacheCharacter.GetCharacterAnimator() != null)
+                {
+                    if (waitTillComplete)
+                    {
+                        float wait = Time.time + clip.length / 1;
+
+                        WaitUntil waitUntil = new WaitUntil(() => forceStop || Time.time > wait);
+                        yield return waitUntil;
+                    }
+                }
+
+                if (lootTable != null)
+                {
+                    LootTable.LootResult loot = lootTable.Get();
+
+                    if (loot.item != null && loot.amount > 0)
+                    {
+                        InventoryManager.Instance.AddItemToInventory(
+                            loot.item.uuid,
+                            loot.amount
+                        );
+                    }
+                }
+
+                cacheCharacter.characterLocomotion.SetIsControllable(true);
+            }
+
+            yield return 0;
+        }
+
+
+#if UNITY_EDITOR
+
+        public new static string NAME = "Character/Gathering";
+        private const string NODE_TITLE = "Gathering";
+
+        public override string GetNodeTitle()
+        {
+            return string.Format(NODE_TITLE, "", "");
+        }
+
+#endif
+    }
+}
