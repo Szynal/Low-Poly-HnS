a8e88092aa8731458bfdab0ad09f176e36d9c314
Paweł Szynal <pawel.szynal@email.com>
2020-05-09 10:37:00 +0200

    Create ICraftingUIItem.cs

---
+++ Assets/Scripts/LowPolyHnS/Inventory/Mono/UI/Crafting/ICraftingUIItem.cs
@@ -0,0 +1,33 @@
+﻿using LowPolyHnS.Inventory;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LowPolyHnS.Crafting
+{
+    public abstract class ICraftingUIItem : MonoBehaviour
+    {
+        protected static DatabaseInventory DATABASE_INVENTORY;
+
+        #region PROPERTIES
+
+        protected CraftingUIManager CraftingUIManager;
+
+        public Image Image;
+        public Text TextName;
+        public Text TextDescription;
+        public Text TextPrice;
+
+        #endregion
+
+
+        #region CONSTRUCTOR & UPDATER
+
+        public virtual void Setup(CraftingUIManager craftingUIManager, params object[] parameters)
+        {
+            CraftingUIManager = craftingUIManager;
+        }
+
+        #endregion
+
+    }
+}
