27f0a122e55c1309dbd21f7a28d0d890e85ae1f6
Paweł Szynal <pawel.szynal@email.com>
2020-05-09 10:52:53 +0200

    Update CraftingUIItemPlayer.cs

--- Assets/Scripts/LowPolyHnS/Inventory/Mono/UI/Crafting/CraftingUIItemPlayer.cs
+++ Assets/Scripts/LowPolyHnS/Inventory/Mono/UI/Crafting/CraftingUIItemPlayer.cs
@@ -1,10 +1,42 @@
-﻿using LowPolyHnS.Core.Hooks;
+﻿using System;
+using LowPolyHnS.Inventory;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace LowPolyHnS.Crafting
 {
-    public class CraftingUIItemPlayer
+    public class CraftingUIItemPlayer : CraftingUIItem
     {
+        #region PROPERTIES
+
+        private Item item;
+
+        public GameObject WrapAmount = null;
+        public Text TextAmount = null;
+
+        #endregion
+
+        #region CONSTRUCTOR & UPDATER
+
+        public override void Setup(CraftingUIManager craftingUIManager, params object[] parameters)
+        {
+            base.Setup(craftingUIManager, parameters);
+            item = parameters[0] as Item;
+
+            UpdateUI();
+        }
+
+        #endregion
+
+
+        public override void UpdateUI()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnClickButton()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
