98c0e6cfc8647b1b531c593fe19f3cdfbf0fd650
Paweł Szynal <pawel.szynal@email.com>
2020-05-14 23:57:52 +0200

    Create AttributeModifier.cs

---
+++ Assets/Scripts/LowPolyHnS/Attributes/Mono/AttributeModifier.cs
@@ -0,0 +1,12 @@
+﻿namespace LowPolyHnS.Attributes
+{
+    public class AttributeModifier
+    {
+        public readonly float Value;
+
+        public AttributeModifier(float value)
+        {
+            Value = value;
+        }
+    }
+}
