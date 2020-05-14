46c23832aee11bf71d78c19163f0fed0acf7d947
Paweł Szynal <pawel.szynal@email.com>
2020-05-14 23:57:49 +0200

    Create CharacterAttributes.cs

---
+++ Assets/Scripts/LowPolyHnS/Attributes/Mono/CharacterAttributes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LowPolyHnS.Attributes
+{
+    public class CharacterAttributes
+    {
+        public float BaseValue;
+
+        private readonly List<AttributeModifier> statModifiers;
+
+        public CharacterAttributes(float baseValue)
+        {
+            BaseValue = baseValue;
+            statModifiers = new List<AttributeModifier>();
+        }
+
+        public float Value => CalculateFinalValue();
+
+        public void AddModifier(AttributeModifier attributeModifier)
+        {
+            statModifiers.Add(attributeModifier);
+        }
+
+        public bool RemoveModifier(AttributeModifier attributeModifier)
+        {
+            return statModifiers.Remove(attributeModifier);
+        }
+
+        private float CalculateFinalValue()
+        {
+            float finalValue = BaseValue + statModifiers.Sum(t => t.Value);
+
+            return (float) Math.Round(finalValue, 4);
+        }
+    }
+}
