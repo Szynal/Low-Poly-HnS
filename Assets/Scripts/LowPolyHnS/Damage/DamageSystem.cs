a75facaa7e54eed97ac9c8336e7163ad56b2ac84
Paweł Szynal <pawel.szynal@email.com>
2020-05-17 15:19:51 +0200

    Update DamageSystem.cs

--- Assets/Scripts/LowPolyHnS/Damage/DamageSystem.cs
+++ Assets/Scripts/LowPolyHnS/Damage/DamageSystem.cs
@@ -1,39 +1,62 @@
-﻿using LowPolyHnS.Attributes;
+﻿using System.Globalization;
+using LowPolyHnS.Attributes;
 using LowPolyHnS.Inventory;
 using UnityEngine;
 
 namespace LowPolyHnS.Damage
 {
-    public class DamageSystem : MonoBehaviour
+    public class DamageSystem
     {
+        public float Value { get; }
+
         public enum DamageType
         {
             Physical,
             Magic
         }
 
-        public readonly Item Weapon;
-        public readonly float MinDamageValue;
-        public readonly float MaxDamageValue;
-        public readonly float AverageDamageValue;
-        public readonly float Value;
-
-        public readonly AttributeModifierType Type;
-        public readonly int Order;
+        private Item weapon;
+        private float MinDamageValue { get; }
+        private float MaxDamageValue { get; }
+        private float AverageDamageValue => (MinDamageValue + MaxDamageValue) / 2;
 
         public DamageType TypeOfDamage = DamageType.Physical;
 
-        public DamageSystem(AttributeModifier strength, float value, AttributeModifierType type, int order, Item item)
+        public DamageSystem(AttributeModifier strength, Item item)
         {
-            Value = PhysicalDamageFormula(strength);
-            Type = type;
-            Order = order;
-            Weapon = item;
+            if (weapon == null)
+            {
+                MinDamageValue = 1f;
+                MaxDamageValue = 2f;
+            }
+            else
+            {
+                MinDamageValue = weapon.MinDamageValue;
+                MaxDamageValue = weapon.MaxDamageValue;
+            }
+
+            Value = PhysicalDamageFormula(strength, item);
+            weapon = item;
         }
 
-        public float PhysicalDamageFormula(AttributeModifier strength)
+        public float PhysicalDamageFormula(AttributeModifier strength, Item item)
         {
-            return (Random.Range(MinDamageValue, MaxDamageValue) * (strength.Value / 10)) ;
+            return Random.Range(MinDamageValue, MaxDamageValue) * (strength.Value / 10);
+        }
+
+        public string GetMinDamageValueInfo()
+        {
+            return MinDamageValue.ToString(CultureInfo.InvariantCulture);
+        }
+
+        public string GetMaxDamageValueInfo()
+        {
+            return MinDamageValue.ToString(CultureInfo.InvariantCulture);
+        }
+
+        public string GetAverageDamageValue()
+        {
+            return AverageDamageValue.ToString(CultureInfo.InvariantCulture);
         }
     }
 }
